5c5faaf2ad16fa3237f64612eac2f76bb4ab1ebc
Osher El-Netanany <osher@perimeterx.com>
2015-09-06 18:07:30 +0300

    works in dev, does not work in CI mode

---
+++ test/client/px.test.js
@@ -0,0 +1,8 @@
+describe("client", function() {
+  it("is cool", function() { } );
+  it("is awsome", function() { } );
+  
+  context("client utility", function() { 
+    it("has no bugs", function() {});
+  })
+})
---
+++ test/server/lib.web.test.js
@@ -0,0 +1,7 @@
+describe("server", function() {
+  it("is cool", function() { } );
+  it("is very cool", function() { } );
+  context("/service", function() { 
+    it("service is also cool", function() {} );
+  })
+})
